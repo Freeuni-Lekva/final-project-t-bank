1ed41b15c397541d5b6a3782b4c5dbbbf33ddf45
gnatenadze <gnate18@freeuni.edu.ge>
2021-08-09 23:06:52 +0400

    feat(EventsList): added Events list prototype

---
+++ T-Bank/src/main/java/com/example/T_Bank/EventsListServlet.java
@@ -0,0 +1,50 @@
+package com.example.T_Bank;
+
+import com.example.T_Bank.DAO.TBankDAO;
+import com.example.T_Bank.Storage.Account;
+import com.example.T_Bank.Storage.AccountNumbersList;
+import com.example.T_Bank.Storage.CrowdFundingEvent;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+@WebServlet(name = "EventsListServlet", value = "/EventsListServlet")
+public class EventsListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext context = getServletContext();
+        TBankDAO tBank = (TBankDAO) context.getAttribute("TBankDAO");
+
+        Map<String, Account> sessions = (Map<String, Account>) context.getAttribute("Sessions");
+        Account account = sessions.get(request.getSession().getId());
+
+        ArrayList<CrowdFundingEvent> events = tBank.getPublicCrowdFundingEvents();
+        request.setAttribute("events", events);
+
+        AccountNumbersList senderList = tBank.getAccountNumbers(account.getPersonalId());
+        request.setAttribute("senderAccounts", senderList.getAccountNumbers());
+
+        request.getRequestDispatcher("EventsListPage.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext context = getServletContext();
+        TBankDAO tBank = (TBankDAO) context.getAttribute("TBankDAO");
+
+        Map<String, Account> sessions = (Map<String, Account>) context.getAttribute("Sessions");
+        Account account = sessions.get(request.getSession().getId());
+
+        ArrayList<CrowdFundingEvent> events = tBank.getPublicCrowdFundingEvents();
+        request.setAttribute("events", events);
+
+        AccountNumbersList senderList = tBank.getAccountNumbers(account.getPersonalId());
+        request.setAttribute("senderAccounts", senderList.getAccountNumbers());
+
+        request.getRequestDispatcher("EventsListPage.jsp").forward(request, response);
+    }
+}
